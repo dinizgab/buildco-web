adb95bb00ae73e543cd1f339a76beada5e8a6602
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-03-28 17:03:28 -0300

    feat: adding email and phone icons

--- src/app/components/company_card.tsx
+++ src/app/components/company_card.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Phone, At } from "@phosphor-icons/react"
 
 type CompanyCardProps = {
@@ -6,15 +7,16 @@ type CompanyCardProps = {
     email: string
 }
 
-export default async function CompanyCard({ name, phone, email }: CompanyCardProps) {
+export default function CompanyCard({ name, phone, email }: CompanyCardProps) {
     return (
-        <div className="w-1/4 h-fit text-black bg-gray-300 rounded-2xl p-4">
-            <h1 className="mb-6 font-semibold text-3xl">{name}</h1>
-
+        <div className="w-1/4 h-fit text-black bg-gray-300 rounded-2xl p-4 mb-4">
+            <h1 className="mb-2 font-semibold text-3xl">{name}</h1>
             <div className="w-3/4 mb-2 flex flex-row items-center">
+                <At size={24}/>
                 <p className="text-lg ml-2">{email}</p>
             </div>
             <div className="w-3/4 flex flex-row items-center">
+                <Phone size={24}/>
                 <p className="text-lg ml-2">{phone}</p>
             </div>
         </div>
