7c871ae90fa60b94d23695b92384dec652daae3d
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-03-31 21:55:46 -0300

    fix: fixing directory structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import CompanyCard from "./components/company_card";
-import Company from "./types/company";
+import CompanyCard from "../components/company_card";
+import Company from "../types/company";
 
 export default async function Home() {
     const response = await fetch('http://localhost:8000/v1/company')
