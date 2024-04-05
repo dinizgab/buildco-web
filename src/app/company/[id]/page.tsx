0b705c28f912521afacc458a346e137de6e093e9
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-04-04 22:55:04 -0300

    feat: implementing company profile

--- src/app/company/[id]/page.tsx
+++ src/app/company/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function CompanyPage({ params }: Props) {
     const data = await response.json() as Company
 
     return (
-        <div className="flex min-h-screen flex-col items-center">
+        <div className="flex h-screen flex-col items-center">
             <CompanyProfile name={data.name} phone={data.phone} email={data.email} />
 
             <main className="w-1/2 bg-slate-300 rounded-xl">
--- src/components/company_profile.tsx
+++ src/components/company_profile.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { At, InstagramLogo, MapPin, Phone, Star, WhatsappLogo } from "@phosphor-icons/react"
+
 type Props = {
     name: string,
     phone: string,
@@ -7,37 +10,45 @@ type Props = {
 
 export default function CompanyProfile({ name, phone, email }: Props) {
     return (
-        <main className="bg-slate-500 h-80 w-1/2 rounded-xl grid grid-cols-12 gap-5">
-            <div className="col-span-4">
-                imagem
+        <section className="h-3/5 w-1/2 flex flex-col m-auto rounded-3xl">
+            <div className="bg-blue-300">banner</div>
+
+            <div className="flex flex-col w-full bg-red-300 py-6 rounded-b-3xl relative items-center">
+                <div className="rounded-full bg-sky-50 w-64 h-64 absolute -top-32" />
+
+                <div className="flex flex-col items-center h-full gap-4 mt-32">
+                    <div>
+                        <h2 className="font-medium text-3xl text-center">{name}</h2>
+                        <p className="text-gray-500 text-center">Desde 2025</p>
+                    </div>
+                    <div className="rounded-xl bg-slate-300 py-1 px-9">
+                        <p>Campina Grande - PB</p>
+                    </div>
+
+                    <div>
+                        <p className="text-gray-500 text-center">Endereço</p>
+                        <div className="flex flex-row gap-2 items-center">
+                            <MapPin size={32} />
+                            <p className="">Rua da companhia, 123 - Bairro</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <p className="text-gray-500 text-center">Avaliacao</p>
+                        <div className="flex flex-row gap-2 items-center">
+                            <Star size={32} />
+                            <p className="">3/5</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-3">
+                        <Phone size={32} />
+                        <At size={32} />
+                        <InstagramLogo size={32} />
+                        <WhatsappLogo size={32} />
+                    </div>
+
+                </div>
             </div>
-            <div className="pt-20 pb-20 w-full flex flex-col justify-between col-span-2">
-                <div>
-                    <p className="font-light">Nome</p>
-                    <p className="">{name}</p>
-                </div>
-                <div>
-                    <p className="font-light">Telefone</p>
-                    <p className="">{phone}</p>
-                </div>
-            </div>
-            <div className="pt-20 pb-20 w-full flex flex-col justify-between col-span-2">
-                <div>
-                    <p className="font-light">Endereco</p>
-                    <p className="">Rua da companhia, 123 - Construcao</p>
-                </div>
-                <div>
-                    <p className="font-light">Email</p>
-                    <p className="">{email}</p>
-                </div>
-            </div>
-            <div className="pt-20 pb-20 w-full flex flex-col justify-between col-span-2">
-                <div>
-                    <p className="font-light">Avaliacao</p>
-                    <p className="">3.5/5</p>
-                </div>
-            </div>
-            <div className="pt-9 pb-9 w-full flex flex-col justify-between col-span-4">sobre nos</div>
-        </main>
+        </section>
     )
 }
