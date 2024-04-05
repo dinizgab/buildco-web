0875c08651460b430d76afbcc56ed04eced3e156
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-04-05 14:54:34 -0300

    feat: creating comment card

--- src/app/company/[id]/page.tsx
+++ src/app/company/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CommentCard from "@/components/comment_card"
 import CompanyProfile from "@/components/company_profile"
 import Company from "@/types/company"
 
@@ -12,8 +13,9 @@ export default async function CompanyPage({ params }: Props) {
     const data = await response.json() as Company
 
     return (
-        <div className="flex h-screen flex-col items-center">
+        <div className="flex h-screen flex-col items-center gap-5">
             <CompanyProfile name={data.name} phone={data.phone} email={data.email} />
+            <CommentCard userName="Gabriel Diniz" comment={"Testando os comentarios"} commentDate={new Date()} grade={3}/>
         </div>
     )
 }
